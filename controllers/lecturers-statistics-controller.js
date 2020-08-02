a43ce8f47ec202ab0a5034e9167b203f0086170d
Tharinda <tharindarajapakshe@gmail.com>
2020-08-02 22:15:24 +0530

    lecturer statistics controller basic

--- controllers/lecturers-statistics-controller.js
+++ controllers/lecturers-statistics-controller.js
@@ -1,3 +1,86 @@
 const HttpErrorsModel = require('../models/http-errors')
+const LecturerModel = require('../models/lecturers.model')
 
-//
+const addLecturer = async (req, res, next) => {
+  let existingLecturer
+
+  const {
+    LecturerName,
+    EmployeeId,
+    Faculty,
+    Department,
+    Center,
+    Building,
+    Level,
+    Rank
+  } = req.body
+
+  try {
+    existingLecturer = await LecturerModel.findOne({
+      EmployeeId: EmployeeId
+    })
+  } catch (error) {
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  if (existingLecturer) {
+    res.json({
+      exists: true,
+      message: 'A lecturer with the same employee id already exists.'
+    })
+    return next(new HttpErrorsModel('A lecturer with the same employee id already exists.', 409))
+  }
+
+  const newLecturer = new LecturerModel({
+    LecturerName,
+    EmployeeId,
+    Faculty,
+    Department,
+    Center,
+    Building,
+    Level,
+    Rank
+  })
+
+  try {
+    await newLecturer.save()
+  } catch (error) {
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  res.status(201).send({
+    message: 'New lecturer added successfully!'
+  })
+}
+
+const getLecturer = async (req, res, next) => {
+  let lecturer
+
+  const {
+    id
+  } = req.params
+
+  try {
+    lecturer = await LecturerModel.findById(id)
+  } catch (error) {
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  res.status(200).send(lecturer)
+}
+
+const getLecturerList = async (req, res, next) => {
+  let lecturerList
+
+  try {
+    lecturerList = await LecturerModel.find()
+  } catch (error) {
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  res.status(200).send(lecturerList)
+}
+
+exports.addLecturer = addLecturer
+exports.getLecturer = getLecturer
+exports.getLecturerList = getLecturerList
