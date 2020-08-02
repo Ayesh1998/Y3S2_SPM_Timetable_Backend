4d1a9d3b1bc77ead31080de3de79fb24b5456ab4
Tharinda <tharindarajapakshe@gmail.com>
2020-08-02 22:20:03 +0530

    lecturer statistics routes basic

--- routes/lecturers-statistics.routes.js
+++ routes/lecturers-statistics.routes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-//
+const LecturersStatisticsController = require('../controllers/lecturers-statistics-controller')
+
+router.post('/lecturersStatistics', LecturersStatisticsController.addLecturer)
+router.get('/lecturersStatistics/:id', LecturersStatisticsController.getLecturer)
+router.get('/lecturersStatistics', LecturersStatisticsController.getLecturerList)
 
 module.exports = router
