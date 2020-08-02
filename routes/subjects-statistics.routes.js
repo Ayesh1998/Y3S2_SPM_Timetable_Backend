edb30e7805a2a31566b93ec98de92dc42b523f51
Tharinda <tharindarajapakshe@gmail.com>
2020-08-02 22:23:09 +0530

    subjects statistics routes basic

--- routes/subjects-statistics.routes.js
+++ routes/subjects-statistics.routes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-//
+const SubjectsStatisticsController = require('../controllers/subjects-statistics-controller')
+
+router.post('/subjectsStatistics', SubjectsStatisticsController.addSubject)
+router.get('/subjectsStatistics/:id', SubjectsStatisticsController.getSubject)
+router.get('/subjectsStatistics', SubjectsStatisticsController.getSubjectList)
 
 module.exports = router
