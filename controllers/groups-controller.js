1101f173e52bc2163958d15a3337833190cfab88
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-09-18 15:50:33 +0530

    delete fixes

--- controllers/groups-controller.js
+++ controllers/groups-controller.js
@@ -1,9 +1,18 @@
 const HttpError = require('../models/http-errors')
 const Groups = require('../models/groups.model')
+const SubGroups = require('../models/sub-groups.model')
 
 const createGroups = async (req, res, next) => {
-  const {academicYear, academicSemester, academicYearAndSemester, programme, group, groupId, subGroups, availableSubGroup} = req.body
-
+  const {
+    academicYear,
+    academicSemester,
+    academicYearAndSemester,
+    programme,
+    group,
+    groupId,
+    subGroups,
+    availableSubGroup
+  } = req.body
   const GroupsItem = new Groups({
     academicYear,
     academicSemester,
@@ -14,7 +23,6 @@ const createGroups = async (req, res, next) => {
     subGroups,
     availableSubGroup
   })
-
   try {
     await GroupsItem.save()
   } catch (err) {
@@ -22,7 +30,6 @@ const createGroups = async (req, res, next) => {
     res.json({message: 'Adding failed, please try again.', added: 0})
     return next(error)
   }
-
   res.status(201).json({
     groupsItem: GroupsItem.toObject({getters: true}),
     message: 'Added Successfully',
@@ -30,7 +37,7 @@ const createGroups = async (req, res, next) => {
   })
 }
 
-const getGroups = async (req, res, next) => {
+const getGroups = async (req, res) => {
   Groups.find({})
     .then((groups) =>
       res.json({groups: groups, message: 'got results'})
@@ -38,7 +45,7 @@ const getGroups = async (req, res, next) => {
     .catch((err) => res.status(400).json('Error: ' + err))
 }
 
-const editGroups = async (req, res, next) => {
+const editGroups = async (req, res) => {
   const {groups, id} = req.body
   const query = {'_id': id}
   Groups.findOneAndUpdate(query, groups, {upsert: true}, (err, item) => {
@@ -47,41 +54,53 @@ const editGroups = async (req, res, next) => {
   })
 }
 
-const deleteGroups = async (req, res, next) => {
-  const {id} = req.body
-  Groups.findByIdAndDelete((id), {}, (err, item) => {
-    if (err) return res.status(500).send(err)
-  })
-}
-
 const getGroup = async (req, res, next) => {
   let group
-
   const {
     id
   } = req.params
-
   try {
     group = await Groups.findById(id)
   } catch (error) {
     console.log(error)
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500))
   }
-
   res.status(200).send(group)
 }
 
-const deleteSubGroupUpdate = async (req, res, next) => {
-  const {subKey} = req.body
+const deleteGroups = async (req, res) => {
+  let subGroups
+  const {
+    id,
+    groupId
+  } = req.body
+  subGroups = await SubGroups.find({
+    groupId: groupId
+  })
+  for (let i = 0; i < subGroups.length; i++) {
+    await subGroups[i].remove()
+  }
+  await Groups.findByIdAndDelete((id), (err) => {
+    if (err) return res.status(500).send(err)
+  })
+}
 
-  Groups.findOneAndUpdate(
+const deleteSubGroupUpdate = async (req) => {
+  let sub
+  const {
+    subKey,
+    subId
+  } = req.body
+  sub = await SubGroups.findOne({
+    subGroupId: subId
+  })
+  await sub.remove()
+  await Groups.findOneAndUpdate(
     {},
     {$pull: {subGroups: {_id: subKey}}},
-    {multi: true}
-  )
+    {multi: true})
     .then(subGroups => console.log(subGroups))
     .catch(err => console.log(err))
-
 }
 
 exports.createGroups = createGroups
