bc0a20a78b69cfcfde8e335758fe674090d76a66
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-09-20 16:15:01 +0530

    rooms add

--- controllers/add-rooms-controller.js
+++ controllers/add-rooms-controller.js
@@ -1,3 +1,107 @@
-// const HttpErrorsModel = require('../models/http-errors')
+const HttpErrorsModel = require('../models/http-errors')
+const RoomModel = require('../models/rooms.model')
+const BuildingModel = require('../models/buildings.model')
+const TagModel = require('../models/tags.model')
+const SubjectModel = require('../models/subjects.model')
+const LecturerModel = require('../models/lecturers.model')
+const GroupModel = require('../models/groups.model')
+const SubGroupModel = require('../models/sub-groups.model')
 
-//
+const getRoomList = async (req, res, next) => {
+  let roomList
+
+  try {
+    roomList = await RoomModel.find()
+  } catch (error) {
+    console.log(error)
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  res.status(200).send(roomList)
+}
+
+const getBuildingList = async (req, res, next) => {
+  let buildingList
+
+  try {
+    buildingList = await BuildingModel.find()
+  } catch (error) {
+    console.log(error)
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  res.status(200).send(buildingList)
+}
+
+const getTagList = async (req, res, next) => {
+  let tagList
+
+  try {
+    tagList = await TagModel.find()
+  } catch (error) {
+    console.log(error)
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  res.status(200).send(tagList)
+}
+
+const getSubjectList = async (req, res, next) => {
+  let subjectList
+
+  try {
+    subjectList = await SubjectModel.find()
+  } catch (error) {
+    console.log(error)
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  res.status(200).send(subjectList)
+}
+
+const getLecturerList = async (req, res, next) => {
+  let lecturerList
+
+  try {
+    lecturerList = await LecturerModel.find()
+  } catch (error) {
+    console.log(error)
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  res.status(200).send(lecturerList)
+}
+
+const getGroupList = async (req, res, next) => {
+  let groupList
+
+  try {
+    groupList = await GroupModel.find()
+  } catch (error) {
+    console.log(error)
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  res.status(200).send(groupList)
+}
+
+const getSubGroupList = async (req, res, next) => {
+  let subGroupList
+
+  try {
+    subGroupList = await SubGroupModel.find()
+  } catch (error) {
+    console.log(error)
+    return next(new HttpErrorsModel('Unexpected internal server error occurred, please try again later.', 500))
+  }
+
+  res.status(200).send(subGroupList)
+}
+
+exports.getRoomList = getRoomList
+exports.getBuildingList = getBuildingList
+exports.getTagList = getTagList
+exports.getSubjectList = getSubjectList
+exports.getLecturerList = getLecturerList
+exports.getGroupList = getGroupList
+exports.getSubGroupList = getSubGroupList
--- routes/add-rooms.routes.js
+++ routes/add-rooms.routes.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-//
+const AddRoomsController = require('../controllers/add-rooms-controller')
+
+router.get('/rooms', AddRoomsController.getRoomList)
+router.get('/buildings', AddRoomsController.getBuildingList)
+router.get('/tags', AddRoomsController.getTagList)
+router.get('/subjects', AddRoomsController.getSubjectList)
+router.get('/lecturers', AddRoomsController.getLecturerList)
+router.get('/groups', AddRoomsController.getGroupList)
+router.get('/subGroups', AddRoomsController.getSubGroupList)
 
 module.exports = router
