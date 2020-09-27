a933d390c5e6b964a8a477db168bb0657c1a7309
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-09-27 18:33:28 +0530

    possible rooms methods added to routes

--- routes/rooms-for-sessions.routes.js
+++ routes/rooms-for-sessions.routes.js
@@ -7,5 +7,9 @@ router.post('/roomsForSessions', RoomsForSessionsController.addSession)
 router.get('/roomsForSessions/:id', RoomsForSessionsController.getSession)
 router.get('/roomsForSessions', RoomsForSessionsController.getSessionList)
 router.post('/addRoomForSession', RoomsForSessionsController.addRoomForSession)
+router.get('/rooms', RoomsForSessionsController.getRoomList)
+router.get('/getPossibleRoomsForSession/:id', RoomsForSessionsController.getPossibleRoomsForSession)
+router.post('/setPossibleRoomsForSession/:id', RoomsForSessionsController.setPossibleRoomsForSession)
+router.get('/setPossibleRoomsForSessions', RoomsForSessionsController.setPossibleRoomsForSessions)
 
 module.exports = router
