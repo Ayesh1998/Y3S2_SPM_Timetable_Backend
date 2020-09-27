665f83d75827e68122942e91aed409dc29b4e165
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-09-27 18:39:39 +0530

    added unavailability to models

--- models/sessions.model.js
+++ models/sessions.model.js
@@ -134,6 +134,27 @@ const SessionsSchema = new Schema({
     unique: false,
     trim: true
   },
+  unavailability: [{
+    day: {
+      type: String,
+      enum: days,
+      required: false,
+      unique: false,
+      trim: true
+    },
+    startTime: {
+      type: String,
+      required: false,
+      unique: false,
+      trim: true
+    },
+    endTime: {
+      type: String,
+      required: false,
+      unique: false,
+      trim: true
+    }
+  }],
   possibleRooms: [{
     roomName: {
       type: String,
