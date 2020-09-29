2ff5d022362e094edc04d00929a8267e833490fc
harshani2427 <harshaninimanthika2427@gmail.com>
2020-09-29 17:23:39 +0530

    created add consecutive sessions

---
+++ routes/consecutive-sessions.routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const ConsecutiveSessionController = require('../controllers/consecutive-sessions-controller')
+
+router.post('/addConsecutiveSession/:id', ConsecutiveSessionController.addConsecutiveSession)
+
+
+module.exports = router
+ 
