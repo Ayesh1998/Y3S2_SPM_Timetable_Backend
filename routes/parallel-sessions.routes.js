c43bc76ad749c6b6a698ac8257320a38298e1388
harshani2427 <harshaninimanthika2427@gmail.com>
2020-10-02 00:36:08 +0530

    create logic for get category count

--- routes/parallel-sessions.routes.js
+++ routes/parallel-sessions.routes.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const ParallelSessionController = require('../controllers/parallel-sessions-controller')
 
 router.post('/addParallelSession/:id', ParallelSessionController.addParallelSession)
+router.get('/getCategories', ParallelSessionController.getCategories)
+router.post('/getCategoryCount', ParallelSessionController.getCategoryCount)
 
 module.exports = router
